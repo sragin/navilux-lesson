6a2133fd5891a156b80e6c929d28dc52a142a86e
Jongpil Kim <sragin@naver.com>
2018-01-12 12:21:28 +0900

    LDMFB를 POP으로 변경, {}^ 명령은 POP으로 변경불가 push를 대문자로 변경

--- lesson_04/start.s
+++ lesson_04/start.s
@@ -30,15 +30,15 @@ _reset:
    b _reset   /* Jump to _reset if kernel return */
 
 software_interrupt:
-    push	{r0-r12,r14}
+    PUSH	{r0-r12,r14}
     mrs     r1, spsr
-    push	{r1}
+    PUSH	{r1}
     ldr     r0, [lr, #-4]
     bic     r0, r0, #0xff000000
     bl      swiHandler
-    ldmfd   sp!, {r1}
+    POP		{r1}
     msr     spsr_cxsf, r1
-    ldmfd   sp!, {r0-r12,pc}^
+    LDMFD	sp!,{r0-r12,pc}^
 
 IRQ:
     b irqHandler
