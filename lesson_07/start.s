2f2ac6d342edc451c10a446c62fca9621fab984d
Jongpil Kim <sragin@naver.com>
2018-02-06 00:12:59 +0900

    lesson 05 업데이트 된 내용 반영

--- lesson_07/start.s
+++ lesson_07/start.s
@@ -17,11 +17,12 @@ _start:
    b .        /* Data Abort : Stay here            */
    b .        /* Reserved : Stay Here              */
    b IRQ      /* Normal Interrupt : Stay Here      */
-   b IRQ      /* Fast Interrupt : Stay Here        */
+   b _start   /* Fast Interrupt : Reset            */
 
-.equ svc_stack, 0x7A80000
-.equ irq_stack, 0x7B00000
-.equ sys_stack, 0x7B80000
+/* 각각 512K씩 스택 공간을 확보함 0x80000 = 512K */
+.equ svc_stack, 0x0300000
+.equ irq_stack, 0x0380000
+.equ sys_stack, 0x0400000 /* 0x0400000 = 4MB */
 .global kernel_init
 kernel_init:
 	ldr		sp, =stack_top // stack size = 4MB (0x40,0000)
@@ -40,29 +41,19 @@ kernel_init:
 
 	msr		cpsr_c, #0xc0|0x13
 
-   bl  main   /* Jump to main function (C code) */
-   b _start   /* Jump to _reset if kernel return */
+	bl		main   /* Jump to main function (C code) */
+	b		_start   /* Jump to _reset if kernel return */
 
 swi:
     stmfd   sp!, {r0-r12,r14}
-    mrs     r1, spsr
-    stmfd   sp!, {r1}
     ldr     r0, [lr, #-4]
     bic     r0, r0, #0xff000000
     bl      swiHandler
-    ldmfd   sp!, {r1}
-    msr     spsr_cxsf, r1
     ldmfd   sp!, {r0-r12,pc}^
 
 IRQ:
     sub     lr, lr, #4
-    stmfd   sp!, {lr}
-    stmfd   sp!, {r0-r14}^
-    mrs     r1, spsr
-    stmfd   sp!, {r1}
+    stmfd   sp!, {r0-r12,lr}
     bl      irqHandler
-    ldmfd   sp!, {r1}
-    msr     spsr_cxsf, r1
-    ldmfd   sp!, {r0-r14}^
-    ldmfd   sp!, {pc}^
+    ldmfd   sp!, {r0-r12,pc}^
 
